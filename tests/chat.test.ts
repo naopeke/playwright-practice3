5e2c0cf561f73368fe3b849b0ea6f5e1e293cdd2
naopeke <naopeke@protonmail.com>
2024-10-16 16:00:22 +0200

    chat

--- tests/chat.test.ts
+++ tests/chat.test.ts
@@ -1,18 +1,29 @@
-import { test, chromium } from '@playwright/test';
+import { test, chromium, expect } from '@playwright/test';
 
 test.use({ headless: false })
 
-test('chat testing', async({ page })=>{
+test.describe('Chat testing', ()=>{
 
-    const browerInstance = await chromium.launch();
+    test('chat testing_01', async({ page })=>{
     
-    const automationUser1Context = await browerInstance.newContext();
-    const automationUser2Context = await browerInstance.newContext();
+        const browerInstance = await chromium.launch();
+        
+        const automationUser1Context = await browerInstance.newContext();
+        const automationUser2Context = await browerInstance.newContext();
+        
+        const automationUser1Page = await automationUser1Context.newPage();
+        const automationUser2Page = await automationUser2Context.newPage();
     
-    const automationUser1Page = await automationUser1Context.newPage();
-    const automationUser2Page = await automationUser2Context.newPage();
+        await automationUser1Page.goto('');
+        await automationUser1Page.getByPlaceholder('channel').fill('naopeke');
+        await automationUser1Page.locator('#join_button').click();
+        await expect(automationUser1Page).toHaveURL('/naopeke', { timeout: 5000 });
+        //toHaveValue()は<input>, <textarea>, toHaveText()は<div><span>
+        await expect(automationUser1Page.locator('#user-counter')).toHaveText('1');
 
-    await automationUser1Page.goto('');
-    await automationUser1Page.getByPlaceholder('channel').fill('naopeke');
+    })
+
+
 })
+
 
