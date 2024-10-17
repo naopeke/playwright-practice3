ac72d6a0de973442977b4216735ef9815714d3ea
naopeke <naopeke@protonmail.com>
2024-10-17 15:28:37 +0200

    chat

--- tests/chat.test.ts
+++ tests/chat.test.ts
@@ -32,7 +32,25 @@ test.describe('Chat testing', ()=>{
     })
 
     test('chat testing_03', async ({ page })=>{
+        const browerInstance = await chromium.launch();
+        const userContext1 = await browerInstance.newContext();
+        const userPage1 = await userContext1.newPage();
+        const userContext2 = await browerInstance.newContext();
+        const userPage2 = await userContext2.newPage();
+    
+        await userPage1.goto('');
+        await userPage1.getByPlaceholder('channel').fill('naopeke');
+        await userPage1.locator('#join_button').click();
+        await expect(userPage1).toHaveURL('/naopeke', { timeout: 5000 });
+        //toHaveValue()は<input>, <textarea>, toHaveText()は<div><span>
+        await expect(userPage1.locator('#user-counter')).toHaveText('1', { timeout: 5000 });
 
+        await userPage2.goto('/naopeke');
+        await userPage2.getByPlaceholder('Name').fill('Taro');
+        const nameInput =  userPage2.getByPlaceholder('Name');
+        await nameInput.focus();
+        await nameInput.press('Enter');
+        await expect(userPage2.locator('#user-counter')).toHaveText('2', { timeout: 5000 });
     })
 })
 
